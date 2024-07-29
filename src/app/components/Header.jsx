3c2e772e93202fddd84483b4d9c6cfd75d9b9d1e
Adam Evans <adam3van5@gmail.com>
2024-07-29 20:46:28 +0900

    add header

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Children } from 'react';
+
+const Header = ({ children }) => {
+  return (
+    <header className="flex items-center justify-between space-x-4 p-6">
+      <div className="flex items-center space-x-4">{children}</div>
+    </header>
+  );
+};
+
+export default Header;
