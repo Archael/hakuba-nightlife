97cbf600eff7592d84c15a540185eb2aadd1cd15
Adam Evans <adam3van5@gmail.com>
2024-07-29 20:48:17 +0900

    add header to page

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Children } from 'react';
 
-const Header = ({ children }) => {
+const Header = () => {
   return (
     <header className="flex items-center justify-between space-x-4 p-6">
       <div className="flex items-center space-x-4">{children}</div>
