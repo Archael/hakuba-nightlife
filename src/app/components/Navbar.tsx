f7137c3137ea53cbd52fe0e2d4287d0cdb779f5b
Adam Evans <adam3van5@gmail.com>
2024-08-01 08:17:56 +0900

    add navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,88 @@
+import './navbar.css';
+
+const links = [
+  {
+    name: 'Link 1',
+    href: '#',
+  },
+  {
+    name: 'Link 2',
+    href: '#',
+  },
+  {
+    name: 'Link 3',
+    href: '#',
+  },
+  {
+    name: 'Link 4',
+    href: '#',
+  },
+  {
+    name: 'Link 5',
+    href: '#',
+  },
+];
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <header className="navbar-container">
+        <header data-thq="thq-navbar" className="navbar-navbar-interactive">
+          <div className="navbar-image1">
+            <img alt="image" src="/assets/logo.svg" className="navbar-logo" />
+          </div>
+          <div data-thq="thq-navbar-nav" className="navbar-desktop-menu">
+            <nav className="navbar-links">
+              <span className="thq-body-small thq-link">link 1</span>
+              <span className="thq-body-small thq-link">link 2</span>
+              <span className="thq-body-small thq-link">link 3</span>
+              <span className="thq-body-small thq-link">link 4</span>
+              <span className="thq-body-small thq-link">link 5</span>
+            </nav>
+            <div className="navbar-buttons">
+              <button className="navbar-action1 thq-button-filled thq-button-animated">
+                <span className="thq-body-small">Action 1</span>
+              </button>
+              <button className="navbar-action2 thq-button-outline thq-button-animated">
+                <span className="thq-body-small">Action 2</span>
+              </button>
+            </div>
+          </div>
+          <div data-thq="thq-burger-menu" className="navbar-burger-menu">
+            <svg viewBox="0 0 1024 1024" className="navbar-icon">
+              <path d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
+            </svg>
+          </div>
+          <div data-thq="thq-mobile-menu" className="navbar-mobile-menu">
+            <div className="navbar-nav">
+              <div className="navbar-top">
+                <img
+                  alt="image"
+                  src="/assets/logo.svg"
+                  className="navbar-logo"
+                />
+                <div data-thq="thq-close-menu" className="navbar-close-menu">
+                  <svg viewBox="0 0 1024 1024" className="navbar-icon2">
+                    <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
+                  </svg>
+                </div>
+              </div>
+              <nav className="navbar-links1">
+                <span className="thq-body-small thq-link">link 1</span>
+                <span className="thq-body-small thq-link">link 2</span>
+                <span className="thq-body-small thq-link">link 3</span>
+                <span className="thq-body-small thq-link">link 4</span>
+                <span className="thq-body-small thq-link">link 5</span>
+              </nav>
+            </div>
+            <div className="navbar-buttons1">
+              <button className="thq-button-filled">Login</button>
+              <button className="thq-button-outline">Register</button>
+            </div>
+          </div>
+        </header>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
