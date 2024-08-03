2392e011e142c8c1283526bfe80d3761cfd036c6
Adam Evans <adam3van5@gmail.com>
2024-08-03 12:25:00 +0900

    add footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import CallToAction from './components/Cta/CallToAction';
+import Footer from './components/Footer/Footer';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </Header>
       <Hero />
       <CallToAction />
+      <Footer />
     </div>
   );
 }
