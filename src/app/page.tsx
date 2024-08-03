430dbe57db195de492a5e6f67f74bf892fe9feb0
Adam Evans <adam3van5@gmail.com>
2024-08-03 11:12:10 +0900

    restructure components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
 
 export default function Home() {
   return (
