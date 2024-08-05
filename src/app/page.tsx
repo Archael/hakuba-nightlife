35a05c095abeb7766e25fd16d2fce312a776c171
Adam Evans <adam3van5@gmail.com>
2024-08-05 21:09:28 +0900

    add contact template code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import CallToAction from './components/CallToAction/CallToAction';
+import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       </Header>
       <Hero />
       <CallToAction />
+      <Contact />
       <Footer />
     </div>
   );
